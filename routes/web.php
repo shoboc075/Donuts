feff7c21d8164f836c9ff28c82f2e8199a2958f2
shohei.imamura <shoboc075@gmail.com>
2018-06-24 10:13:27 +0000

    2018/6/24_UserLogin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ Route::get('/', function() {
     return view('1_PreLogin.welcome');
 });
 
+// Login authentication
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+
 Route::resource('hobbies', 'HobbiesController');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
